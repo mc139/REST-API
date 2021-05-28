d9b2f5570306c2d85f150c84f03336427dab7d00
mc139 <maciejandrzejcieslik@gmail.com>
2021-05-28 13:47:27 +0100

    showtask.sh added

---
+++ showtask.sh
@@ -0,0 +1,9 @@
+ chmod +x runcrud.sh
+./runcrud.sh
+if chmod runcrud.sh; then
+  echo "Success"
+else
+  echo "There were errors"
+fi
+
+open -a "Google Chrome" http://localhost:8080/crud/v1/task/getTasks
